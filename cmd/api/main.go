7128ec1ded4799f7831913bf15c20b04e9e06f85
johernandezvaz <johernandezvaz@gmail.com>
2026-09-18 10:16:12 -0600

    Crear ruta health

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/johernandezvaz/devboard/internal/server"
 )
@@ -11,9 +12,16 @@ func main() {
 	// Aqui arranca el servidor
 	srv := server.New(":8080")
 
+	srv.RegisterRoutes("GET /health", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		writer.Write([]byte(`{"status": "ok"}`))
+	}))
+
 	log.Println("Servicio funcionando en :8080")
 
 	if err := srv.Start(); err != nil {
 		log.Fatalf("Error al iniciar el servidor %v", err)
 	}
+
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,3 +1,4 @@
+// Package server creates a new HTTP server
 package server
 
 import (
@@ -5,11 +6,13 @@ import (
 	"time"
 )
 
+// Server struct
 type Server struct {
 	httpServer *http.Server
 	mux        *http.ServeMux
 }
 
+// New create a new server
 func New(addr string) *Server {
 	mux := http.NewServeMux() // Entrutador HTTP
 
@@ -30,6 +33,17 @@ func New(addr string) *Server {
 
 }
 
+// Start server
 func (s *Server) Start() error {
 	return s.httpServer.ListenAndServe()
+}
+
+// RegisterRoutes register a new route
+func (s *Server) RegisterRoutes(pattern string, handler http.Handler) {
+	s.mux.Handle(pattern, handler)
+}
+
+// ServeHTTP server
+func (s *Server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	s.mux.ServeHTTP(writer, request)
 }
